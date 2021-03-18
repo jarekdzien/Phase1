127ade59d8796d5428ea55e8370aedc1ac02dd8d
d3athstaar <d3athstaar@outlook.com>
2021-03-18 17:43:24 +0000

    4th commit

---
+++ Phase1Section6.9/Phase1Section6.9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Phase1Section6._9
+{
+    public class MyPackage
+    {
+        public void displayText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void displayDate()
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+        }
+    }
+}
